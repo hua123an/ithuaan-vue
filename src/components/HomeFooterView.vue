4bed4c9dfeef75a211849005f5571837e6a4744d
人间惊鸿宴 <1994843581@qq.com>
2023-08-28 00:11:01 +0800

    first commit
    first commit

---
+++ src/components/HomeFooterView.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    <span>版权所有 © ithuaan</span>
+
+  </footer>
+
+</template>
+
+<style scoped>
+*{
+  width: 100%;
+  height: 100px;
+  background: rgb(128, 128, 128);
+}
+span{
+  display: block;
+  color: rgb(255, 255, 255);
+  padding-top: 50px;
+  font-size: 20px;
+  font-weight: 700;
+  text-align: center;
+  justify-content: center;
+  justify-items: center;
+}
+
+</style>
---
+++ src/components/HomeHeaderView.vue
@@ -0,0 +1,72 @@
+<script setup>
+import {ref} from "vue"
+const tabList = ref([
+  {
+    "key" : '1',
+    "name" : 'Home',
+    "to" : '/'
+  },
+  {
+    "key" : '2',
+    'name' : 'AI',
+    "to" : "ai"
+  },
+  {
+    "key" : "3",
+    "name" : 'Blog',
+    "to" : 'blog'
+  },
+  {
+    "key" : '4',
+    'name' : 'Academic',
+    "to" : 'academic'
+  },
+  {
+    "key" : '5',
+    "name" : 'Programming',
+    "to" : 'programming'
+  },
+  {
+    "key" : '6',
+    "name" : 'University',
+    "to" : 'university'
+  },
+  {
+    "key" : '7',
+    "name" : 'Resources',
+    "to" : 'resources'
+  },
+  {
+    "key" : '8',
+    "name" : 'Key',
+    "to" : 'key'
+  },
+  {
+    "key" : '9',
+    "name" : 'OpenSource',
+    "to" : 'opensource'
+  }
+])
+</script>
+<template>
+  <div class="menu-demo">
+    <a-menu mode="horizontal"   :default-selected-keys=" `v-if= item.key == 1`" >
+      <RouterLink :to="item.to" v-for="item in tabList">
+      <a-menu-item :key="item.key" class="menu-item" >{{item.name}}</a-menu-item>
+      </RouterLink>
+    </a-menu>
+  </div>
+</template>
+<style scoped>
+.menu-demo {
+  margin-bottom: 24px;
+  width: auto;
+  justify-items: center;
+  justify-content: center;
+  background: #2c3e50;
+}
+.menu-item{
+  margin-left: 70px;
+}
+
+</style>
